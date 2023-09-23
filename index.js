e298f0ccc89b7c9da112f8a59801a2e5deaddfeb
sai verma <vectorvarma0303@gmail.com>
2023-09-23 16:46:19 +0530

    update

--- index.js
+++ index.js
@@ -1,46 +1,90 @@
-const API_KEY = "your api key";
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector(".weather-container");
+
+const grantAccessContainer = document.querySelector(".grant-location-container")
+const searchForm = document.querySelector("[data-searchForm]")
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
 
 
-function renderWeatherData(data){
-    let newPara = document.createElement("p");
-    newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`;
+let currentTab = userTab;
+const API_KEY = ""
+currentTab.classList.add("current-tab");
 
-    document.body.appendChild(newPara);
-}
+userTab.addEventListener('click', () => {
+    switchTab(userTab);
+});
 
+searchTab.addEventListener('click', () => {
+    switchTab(searchTab);
+});
 
-async function fetchWeatherDetails() {
-    try {
-        let city = "goa";
+function switchTab(clickedtab){
+    if(clickedtab != currentTab){
+        currentTab.classList.remove("current-tab");
+        currentTab = clickedtab;
+        currentTab.classList.add("current-tab")
 
-        const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-        );
-
-        const data = await response.json();
-        // console.log("weather Data:->", data);
-        renderWeatherData(data);
-
-        render
-    } catch (err) {
-        //error should be handled here
+        if(!searchForm.classList.contains("active")){
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.remove("active");
+            searchForm.classList.add("active")
+        } else {
+            searchForm.classList.remove("active")
+            userInfoContainer.classList.remove("active")
+            getfromSessionStorage();
+        }
     }
 }
 
-async function getCustomWeatherDetails(){
-   try{
-    let lat = 15.6333;
-    let lon = 18.3333;
-
-    let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
-    let data = await result.json()
-
-    console.log(data);
-   }
-   catch(err){
-    console.log("error found", err);
-   }
+//check if cordinates are already present in session storage
+function getfromSessionStorage(){
+    const localCoordinates = sessionStorage.getItem("user-coordinates")
+    if(!localCoordinates){
+        //if there are no local cordinates (this means you didnt give location access)
+        grantAccessContainer.classList.add("active")
+    } else {
+        const coordinates = JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates);
+    }
 }
 
+async function fetchUserWeatherInfo(coordinates){
+    const {lat, lon} = coordinates;
+    //make grantcontainer invisible and show loading...
+    grantAccessContainer.classList.remove("active")
+    //show loading..
+    loadingScreen.classList.add("active")
 
+    //api call
+    try{
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`
+        )
 
+        const data = await response.json();
+        loadingScreen.classList.remove("active")
+        userInfoContainer.classList.add("active");
+        rendWeatherInfo(data)
+    }
+    catch(err){
+        
+    }
+
+}
+
+function rendWeatherInfo(data){
+    const cityName = document.querySelector("[data-cityName]")
+    const country = document.querySelector("[data-countryIcon]")
+    const desc = document.querySelector("[data-weatherDesc]")
+    const weatherIcon = document.querySelector("[data-weatherIcon]")
+    const temp = document.querySelector("[data-temp]")
+    const windspeed= document.querySelector("[data-windspeed]")
+    const humidity = document.querySelector("[data-humidity]")
+    const cloudiness = document.querySelector("[data-cloudiness]")
+
+    //fetch values from data and put in ui elements
+    
+
+}
